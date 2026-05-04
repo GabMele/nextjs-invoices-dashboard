02233a58f90382348365c10401c1a9a48f51431d
Gabriele Melendugno <gab.mel.dev@gmail.com>
2026-05-04 17:47:30 +0200

    "Improve logo responsive design and centering
    - Update SideNav and logo component to center logo on mobile, left-align on desktop

--- app/ui/nextquoinvoices-logo.tsx
+++ app/ui/nextquoinvoices-logo.tsx
@@ -1,13 +1,39 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+// import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { lusitana } from "@/app/ui/fonts";
+
+/*
+export default function NextQuoInvoicesLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex h-16 mb-0 text-white md:flex-col h-20`}
+    >
+      <div className="text-4xl md:text-4xl">NextQuo &nbsp;</div>
+      <div className="text-4xl md:text-3xl">Invoices</div>
+    </div>
+  );
+}
+*/
+
+/*
+export default function NextQuoInvoicesLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row place-content-center text-white md:flex-col md:justify-start`}
+    >
+      <div className="text-4xl md:text-4xl">NextQuo</div>
+      <div className="ml-3 text-4xl md:text-3xl md:ml-0">Invoices</div>
+    </div>
+  );
+}
+*/
 
 export default function NextQuoInvoicesLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center justify-center gap-3 text-white md:flex-col md:items-start md:justify-start md:gap-0`}
     >
-      <GlobeAltIcon className="size-12 rotate-[15deg]" />
-      <p className="text-[44px]">NextQuoInvoices</p>
+      <div className="text-4xl">NextQuo</div>
+      <div className="text-4xl md:text-3xl">Invoices</div>
     </div>
   );
 }
